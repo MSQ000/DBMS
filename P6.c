9c4ce3553f67f55f925ec861903fd9035f622380
Mohammed Shamshuddin Qureshi <shamshuddinmsq@gmail.com>
2025-05-17 16:28:46 +0530

    Update P6.c

--- P6.c
+++ P6.c
@@ -1,23 +1,23 @@
 create procedure merge_rollcall_data()
 begin
-    declare n_id int; -- Student ID
-    declare n_name varchar(255); -- Student Name
-    declare n_birth_date date; -- Birth Date
-    declare done int default false; -- Flag to indicate end of cursor
+    declare n_id int;
+    declare n_name varchar(255);
+    declare n_birth_date date;
+    declare done int default false;
     declare n_cursor cursor for 
-    select student_id, student_name, birth_date from N_rollcall; -- Selecting from N_rollcall table
+    select student_id, student_name, birth_date from N_rollcall;
     declare continue handler for not found
-    set done=true; -- Set flag when cursor reaches the end
+    set done=true;
 
     open n_cursor;
     cursor_loop:Loop
     fetch n_cursor into n_id, n_name, n_birth_date;
-    if done then leave cursor_loop; -- Exit loop if cursor is done
+    if done then leave cursor_loop;
     end if;
     if not exists(
     select 1 from O_rollcall where student_id=n_id) then
     insert into O_rollcall(student_id, student_name, birth_date)
-    values(n_id, n_name, n_birth_date); -- Insert new record into O_rollcall
+    values(n_id, n_name, n_birth_date);
     end if;
     end loop;
 
